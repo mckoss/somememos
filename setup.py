bf567226542d15b0424f6f04e713634accb0bd07
Mike Koss <mckoss@startpad.org>
2013-01-30 12:24:28 -0800

    use find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='webnotes',
       version='0.1',
-      packages=['webnotes'],
+      packages=find_packages(),
       install_requires=['tornado>=2.4'],
       )
