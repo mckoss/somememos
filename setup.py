306c3a047d5eac3b699a1539d6b876d8840e8c36
Mike Koss <mckoss@startpad.org>
2013-01-30 20:48:25 -0800

    commad handling

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='webnotes',
-      version='0.1',
+      version='0.1a1',
       packages=find_packages(),
       install_requires=['tornado>=2.4'],
+      author="Mike Koss",
+      author_email="mike@mckoss.com",
+      description="A web application for publishing extensible notebooks.",
+      license="MIT",
+      keywords="tornado web",
+      uri="http://github.com/mckoss/webnotes",
+      scripts=['bin/webnotes'],
       )
